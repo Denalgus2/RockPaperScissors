fc6efcbbf72fef4ea5846d588cd1e7f03f1f3968
Denis Gusev <Denis.ale.gusev@gmail.com>
2024-04-20 16:47:22 +0200

    Updated points system

--- Main.py
+++ Main.py
@@ -1,7 +1,8 @@
 import random
 
 
-def rock_paper_scissors(y_points, c_points):
+def rock_paper_scissors(winner):
+    winner = 0
     user_input = input("Rock, Paper, or Scissors? ").lower()
     choices = ["rock", "paper", "scissors"]
     computer = random.choice(choices)
@@ -10,15 +11,22 @@ def rock_paper_scissors(y_points, c_points):
     elif user_input == "rock" and computer == "scissors" or user_input == "paper" and computer == "rock" or user_input == "scissors" and computer == "paper":
         print("Computer chose " + computer)
         print("You win!")
-        y_points += 1
+        return winner + 1
     else:
         print("Computer chose " + computer)
         print("You lose!")
-        c_points += 1
+        return winner
 
 
 rounds = int(input("How many rounds do you want to play? "))
-
+y_points = 0
+c_points = 0
+win = 0
 for i in range(rounds):
-    rock_paper_scissors(y_points=0, c_points=0)
+    rock_paper_scissors(win)
+    if win == 1:
+        y_points += 1
+    else:
+        c_points += 1
+    print("You: " + str(y_points), "AI: " + str(c_points))
 
